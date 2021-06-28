85eceba4bcfd1350994d5e01546626282d290031
Jerome Doby <xaerodegreaz@gmail.com>
2021-06-28 03:50:30 -0500

    fix formatting

--- src/script2/index.ts
+++ src/script2/index.ts
@@ -1,9 +1,10 @@
 export class Script2 {
-  async main (): Promise<void> {
-    console.log('Hello from script 2 reloaded')
+  async main(): Promise<void>
+  {
+    console.log( 'Hello from script 2 reloaded' )
   }
 }
 
-new Script2().main().catch(e => {
-  console.log(e)
-})
+new Script2().main().catch( e => {
+  console.log( e )
+} )
