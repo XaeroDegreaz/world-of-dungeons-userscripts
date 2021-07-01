70e243f38f59e274f3716acf36bf372ab285f582
Jerome Doby <xaerodegreaz@gmail.com>
2021-07-01 17:24:55 +0900

    fixed notation generator

--- test/Roll.spec.ts
+++ test/Roll.spec.ts
@@ -2,60 +2,69 @@ import expect from 'expect';
 import { describe } from 'mocha';
 import { DiceRoll } from 'rpg-dice-roller';
 
-const roll = ( average: number ): string => {
+const roll = (average: number): string => {
   const maxDiceValue = 15;
   const maxAverageBeforeMax = 7;
-  if ( average <= maxAverageBeforeMax )
-  {
+  if (average <= maxAverageBeforeMax) {
     const diff = maxAverageBeforeMax - average;
     const d = maxDiceValue - diff * 2;
-    return `d${d}`;
+    //# We want the min value to be 0, so we subtract 1 from dice result
+    return `d${d} - 1`;
   }
   //# This flow will only be hit if the average roll is greater than the wrapping point.
   //# Get the whole number of "max dice to use"
-  const numberOfMaxDice = Math.floor( average / maxAverageBeforeMax );
+  const numberOfMaxDice = Math.floor(average / maxAverageBeforeMax);
   //# Get the whole number remaining average that needs to be rolled (this number will be <= maxAverageBeforeMax)
   const remainingAverage = average % maxAverageBeforeMax;
   //# If remaining average = 0 (average was evenly divisible by dice wrap number) do nothing, otherwise roll remainder.
-  const additionalDice = remainingAverage !== 0 ? roll( remainingAverage ) : NaN;
+  const additionalDice = remainingAverage !== 0 ? roll(remainingAverage) : NaN;
   //# For each number of max dice, populate them in the array
-  const largeDice = `${numberOfMaxDice}d${maxDiceValue}`; //[...Array( numberOfMaxDice )].map( ( _ ) => maxDiceValue );
+  //# We subtrace the flat number of max dice from the dice throws such that each roll will be between 0 - X
+  const largeDice = `${numberOfMaxDice}d${maxDiceValue} - ${numberOfMaxDice}`;
   //# If there is any remainder, add that to the end of the dice roll list.
-  return additionalDice ? `${largeDice} + ${additionalDice}` : largeDice; // : largeDice;
+  return additionalDice ? `${largeDice} + ${additionalDice}` : largeDice;
 };
 
 interface R {
   average: number;
-  dice: string;
+  expectedNotation: string;
 }
 
-describe( 't', () => {
+describe('t', () => {
   const values: R[] = [
-    { average: 1, dice: 'd3' },
-    { average: 2, dice: 'd5' },
-    { average: 3, dice: 'd7' },
-    { average: 4, dice: 'd9' },
-    { average: 5, dice: 'd11' },
-    { average: 6, dice: 'd13' },
-    { average: 7, dice: 'd15' },
-    { average: 8, dice: '1d15 + d3' },
-    { average: 9, dice: '1d15 + d5' },
-    { average: 10, dice: '1d15 + d7' },
-    { average: 14, dice: '2d15' },
-    { average: 15, dice: '2d15 + d3' },
-    { average: 16, dice: '2d15 + d5' },
-    { average: 42, dice: '6d15' },
-    { average: 43, dice: '6d15 + d3' },
-    { average: 44, dice: '6d15 + d5' },
-    { average: 45, dice: '6d15 + d7' },
-    { average: 46, dice: '6d15 + d9' },
-    { average: 47, dice: '6d15 + d11' },
+    { average: 1, expectedNotation: 'd3 - 1' },
+    { average: 2, expectedNotation: 'd5 - 1' },
+    { average: 3, expectedNotation: 'd7 - 1' },
+    { average: 4, expectedNotation: 'd9 - 1' },
+    { average: 5, expectedNotation: 'd11 - 1' },
+    { average: 6, expectedNotation: 'd13 - 1' },
+    { average: 7, expectedNotation: 'd15 - 1' },
+    { average: 8, expectedNotation: '1d15 - 1 + d3 - 1' },
+    { average: 9, expectedNotation: '1d15 - 1 + d5 - 1' },
+    { average: 10, expectedNotation: '1d15 - 1 + d7 - 1' },
+    { average: 14, expectedNotation: '2d15 - 2' },
+    { average: 15, expectedNotation: '2d15 - 2 + d3 - 1' },
+    { average: 16, expectedNotation: '2d15 - 2 + d5 - 1' },
+    { average: 42, expectedNotation: '6d15 - 6' },
+    { average: 43, expectedNotation: '6d15 - 6 + d3 - 1' },
+    { average: 44, expectedNotation: '6d15 - 6 + d5 - 1' },
+    { average: 45, expectedNotation: '6d15 - 6 + d7 - 1' },
+    { average: 46, expectedNotation: '6d15 - 6 + d9 - 1' },
+    { average: 47, expectedNotation: '6d15 - 6 + d11 - 1' },
   ];
-  values.forEach( ( x ) => {
-    const { average, dice } = x;
-    it( `Average ${average}=>${dice}`, () => {
-      expect( roll( average ) ).toEqual( dice );
-      console.log( new DiceRoll( dice ).total )
-    } );
-  } );
-} );
+  values.forEach((x) => {
+    const { average, expectedNotation } = x;
+    it(`Average ${average}=>${expectedNotation}`, () => {
+      const actual = roll(average);
+      const diceRoll = new DiceRoll(actual);
+      console.log({
+        total: diceRoll.total,
+        min: diceRoll.minTotal,
+        max: diceRoll.maxTotal,
+        average: diceRoll.averageTotal,
+      });
+      expect(actual).toEqual(expectedNotation);
+      expect(diceRoll.averageTotal).toEqual(average);
+    });
+  });
+});
